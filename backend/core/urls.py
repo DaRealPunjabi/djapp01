b5e3ea80bcd1bd66e894f4fb40acb886d47f296f
DaRealPunjabi <shiv.jalli@gmail.com>
2024-01-04 11:12:00 +0000

    Template04 - navbar

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views import debug
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+#    path('', debug.default_urlconf),
+    path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
 
 ]
