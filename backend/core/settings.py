0601f68a306f673b9bcbc0b6e8b1f2235c8539b1
DaRealPunjabi <shiv.jalli@gmail.com>
2024-01-04 22:14:13 +0000

    Login01

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -145,3 +145,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'user-login'
+
+
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from django.urls import path, include
 from django.views import debug
 
@@ -29,6 +31,10 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 
     path('register/', user_views.register, name='user-registration'),
+    path('profile/', user_views.profile, name='user-profile'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
 
 
 ]
