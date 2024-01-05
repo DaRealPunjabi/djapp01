c45a0a12585063b0d657e31f7488325d6b41cd21
DaRealPunjabi <shiv.jalli@gmail.com>
2024-01-05 14:35:32 +0000

    Password reset

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,10 +13,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR_01 = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIR_02 = os.path.join(BASE_DIR, 'templates/blog')
+
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -140,7 +146,7 @@ MEDIA_URL = '/media/'
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-fieldEMAIL
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -153,4 +159,17 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'user-login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'CodingWithDaRealPunjabi <noreply@noreply.com>'
+
+# print(BASE_DIR)
+# print(dotenv_path)
+# print(EMAIL_HOST)
+
+# print(EMAIL_HOST_USER) 
 
